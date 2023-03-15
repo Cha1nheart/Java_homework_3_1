9b7c95a0f61e570e408ceca7e3cd45425b32fdd5
Cha1nHeart <humanoverrated@gmail.com>
2023-03-15 11:55:58 +0500

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int result = 0;
+        if (price > 19) {
+            result = (price / 20);
+        }
+        return result;
+    }
+}
